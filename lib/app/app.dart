00c3335e658977c2ce9fba32d41d2a4ae21b8722
SK3620 <k.n.t11193620@gmail.com>
2024-12-11 19:35:28 +0900

    Clean: リファクタリング

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:spotify_app/config/routes/routes.dart';
 
 class RiverpodToDoApp extends ConsumerWidget {
