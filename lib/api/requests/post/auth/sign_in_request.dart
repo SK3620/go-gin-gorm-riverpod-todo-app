35da145a7ed668cb638d72c10ac2f527b003246f
SK3620 <k.n.t11193620@gmail.com>
2024-12-13 19:10:40 +0900

    Clean: 不要なファイル、コードを削除＆リファクタリング

--- lib/api/requests/post/auth/sign_in_request.dart
+++ lib/api/requests/post/auth/sign_in_request.dart
@@ -1,15 +1,15 @@
 import 'package:spotify_app/api/service/api_url.dart';
 import 'package:spotify_app/api/service/common_http_router.dart';
 import 'package:spotify_app/model/auth_model.dart';
-import '../../service/http_method.dart';
+import '../../../service/http_method.dart';
 
-class SignUpRequest extends CommonHttpRouter {
+class SignInRequest extends CommonHttpRouter {
   final AuthModel model;
 
-  SignUpRequest(this.model);
+  SignInRequest(this.model);
 
   @override
-  String get path => ApiUrl.signUp;
+  String get path => ApiUrl.signIn;
 
   @override
   HttpMethod get method => HttpMethod.POST;
--- lib/api/requests/post/auth/sign_up_request.dart
+++ lib/api/requests/post/auth/sign_up_request.dart
@@ -1,15 +1,15 @@
 import 'package:spotify_app/api/service/api_url.dart';
 import 'package:spotify_app/api/service/common_http_router.dart';
 import 'package:spotify_app/model/auth_model.dart';
-import '../../service/http_method.dart';
+import '../../../service/http_method.dart';
 
-class SignInRequest extends CommonHttpRouter {
+class SignUpRequest extends CommonHttpRouter {
   final AuthModel model;
 
-  SignInRequest(this.model);
+  SignUpRequest(this.model);
 
   @override
-  String get path => ApiUrl.signIn;
+  String get path => ApiUrl.signUp;
 
   @override
   HttpMethod get method => HttpMethod.POST;
