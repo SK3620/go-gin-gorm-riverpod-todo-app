b974240e88b32e19006096f7f792c8fd5fd430f4
SK3620 <k.n.t11193620@gmail.com>
2024-12-13 19:12:49 +0900

    Add: todoのCRUD用のAPI URLを定義

--- lib/api/service/api_url.dart
+++ lib/api/service/api_url.dart
@@ -3,7 +3,14 @@ class ApiUrl {
   /// 今回はローカルサーバーのみ
   static const String baseUrl = 'localhost::8000hogehoge';
 
-  /// auth
+  /// 認証
   static const String signUp = '/api/sign_up';
   static const String signIn = '/api/sign_in';
+
+  /// ToDo
+  static const String fetchToDos = '/api/to_do/get';
+  static const String addToDo = '/api/to_do/add';
+  static const String editToDo = '/api/to_do/edit';
+  static const String deleteToDo = '/api/to_do/delete';
+  static const String toggleCompletion = '/api/to_do/toggle_completion';
 }
